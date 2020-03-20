ed274490e49e0e2da6f154cecaf846993f6a4fa1
大明 <lmw0827@163.com>
2020-03-20 15:55:25 +0800

    Update has_magic_fields.rb

--- lib/has_magic_fields.rb
+++ lib/has_magic_fields.rb
@@ -4,9 +4,6 @@ require "has_magic_fields/extend"
 module HasMagicFields
   class Engine < ::Rails::Engine
     paths = Dir[File.dirname(__FILE__), '/app/models/**/']
-
-    if Rails.env.production?
-      config.eager_load_paths += paths
-    end
+    config.eager_load_paths += paths
   end
 end
